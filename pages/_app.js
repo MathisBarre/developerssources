9af77c547561c9a3297c23c9c4c20a5802b37220
Mathis Barré <mathis.barre@live.fr>
2020-05-24 15:24:07 +0200

    Home integration

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import "../styles/global.sass"
+import "../styles/normalize.css"
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,184 @@
 import Head from 'next/head'
+import styles from './index.module.sass'
 
-export default function Home() {
+export default function Home({navigation, sources}) {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Developers Learning Depository</title>
       </Head>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <h1 className={styles.headerTitle}>
+            <span className={styles.headerTitleItems}>Developers</span><br />
+            <span className={styles.headerTitleItems}>Learning</span><br />
+            <span className={styles.headerTitleItems}>Depository</span>
+          </h1>
+          <nav className={styles.nav}>
+            {navigation.map(navGroup => (
+              <div className={styles.navGroup}>
+                <h2 className={styles.navGroupTitle}>{navGroup.title}</h2>
+                {navGroup.languages.map(language => (
+                  <NavItem {...language} />
+                ))}
+              </div>
+            ))}
+          </nav>
+        </header>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <main className={styles.main}>
+          <h2 className={styles.mainTitle}>
+            <img className={styles.mainImg} src="/images/javascript.svg" alt=""/>
+            Javascript
+          </h2>
+          {sources.map(source => (
+            <div className={styles.source}>
+              <a className={styles.sourceTitle} href={source.url}>
+                {(source.img) ? <img className={styles.sourceTitleImg} src={source.img} alt=""/> : ""}
+                <span className={styles.sourceTitleText}>{source.title}</span>
+              </a>
+              <div className={styles.sourceBadges}>
+                <span className={styles.sourceBadge}>
+                  <img className={styles.sourceBadgeImg} src="/images/heart.svg" alt=""/>
+                  <span className={styles.sourceBadgeLabel}>{( typeof source.likes === "object") ? source.likes.length : "0" } Likes</span>
+                </span>
+                <span className={styles.sourceBadge}>
+                  <img className={styles.sourceBadgeImg} src="/images/levels.svg" alt="levels"/>
+                  <span className={styles.sourceBadgeLabel}>From {source.level.from} to {source.level.to}</span>
+                </span>
+                {source.languages.map(language => (
+                  <span className={styles.sourceBadge}>
+                    <img className={styles.sourceBadgeImg} src={`/images/${language}.svg`} alt={language}/>
+                    <span className={styles.sourceBadgeLabel}>{language}</span>
+                  </span>
+                ))}
+              </div>
+              <p className={styles.sourceDesc}>{source.desc}</p>
+            </div>
+          ))}
+        </main>
+      </div>
+    </>
+  )
+}
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+function NavItem({img, label}) {
+  return (
+    <div className={styles.navGroupItem}>
+      <img className={styles.navGroupItemImg} src={img} alt="" />
+      <h3 className={styles.navGroupItemLabel} >{label}</h3>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      navigation: [
+        {
+          "title": "Frontend web development",
+          "languages": [
+            {
+              "img": "/images/htmlcss.svg",
+              "label": "HTML & CSS"
+            },
+            {
+              "img": "/images/javascript.svg",
+              "label": "Javascript"
+            },
+            {
+              "img": "/images/react.svg",
+              "label": "React"
+            },
+            {
+              "img": "/images/vuejs.svg",
+              "label": "Vue.js"
+            },
+            {
+              "img": "/images/angular.svg",
+              "label": "Angular"
+            }
+          ]
+        },
+        {
+          "title": "Backend web development",
+          "languages": [
+            {
+              "img": "/images/htmlcss.svg",
+              "label": "HTML & CSS"
+            },
+            {
+              "img": "/images/javascript.svg",
+              "label": "Javascript"
+            },
+            {
+              "img": "/images/react.svg",
+              "label": "React"
+            },
+            {
+              "img": "/images/vuejs.svg",
+              "label": "Vue.js"
+            },
+            {
+              "img": "/images/angular.svg",
+              "label": "Angular"
+            }
+          ]
+        }
+      ],
+      "sources": [
+        {
+          title: "FreeCodeCamp",
+          url: "https://freecodecamp.org",
+          img: "/images/freecodecamp.svg",
+          desc: "Learn to code at home. Build projects. Earn certifications.",
+          languages: [
+            "french",
+            "english"
+          ],
+          likes: [
+            "2020-04-23"
+          ],
+          level: {
+            from: "easy",
+            to: "advanced"
+          },
+          technologies: [
+            "javascript", "html", "css", "react"
+          ],
+          type: [
+            "interactive", "guides", "tutorials"
+          ],
+          payment: {
+            price: false,
+            label: "Only donations"
+          }
+        },
+        {
+          title: "Grafikart",
+          url: "https://grafikart.fr",
+          img: false,
+          desc: "Video courses and written summary about javascript and plenty of others languages",
+          languages: [
+            "french",
+          ],
+          likes: [],
+          level: {
+            from: "easy",
+            to: "advanced"
+          },
+          technologies: [
+            "javascript", "html", "css", "react"
+          ],
+          type: [
+            "interactive", "guides", "tutorials"
+          ],
+          payment: {
+            price: false,
+            label: "Only donations"
+          }
+        }
+      ]
+    }
+  }
 }
