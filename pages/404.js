3f0636bd3c6e2a88a66b938010f03b791579016f
Mathis Barré <mathis.barre@live.fr>
2020-05-31 14:19:29 +0200

    feat: 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout"
+import styles from "./404.module.sass"
+
+export default function page404() {
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <h1 className={styles.errorTitle}>404</h1>
+        <p className={styles.errorDesc}>An error has been occured</p>
+      </main>
+    </Layout>
+  )
+}
