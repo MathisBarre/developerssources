3c3b52d4cdf3a3f777492a7a0cdecc4a6db0768a
Mathis Barré <mathis.barre@live.fr>
2020-05-31 14:20:22 +0200

    change: Home explanations

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,23 @@ export default function Home(props) {
   return (
     <Layout>
       <main className={styles.main}>
-        <h2 className={styles.mainTitle}>Welcome !</h2>
-        <h3>Website aim</h3>
-        <p>This website aims to help developers find good learning sources.</p>
-        <h3>Website contributions</h3>
-        <h4>Contributor list : </h4>
+        <h2 className={styles.mainTitle}>Welcome to developers sources !</h2>
+        <p className={styles.mainDesc}>This website aims to help developers find good learning sources.</p>
+        <h3>Contributions</h3>
+        {/* <h4>Contributor list : </h4>
         <ul>
           <li>Nothing here ! Maybe you will be the first !</li>
+        </ul> */}
+        <p>You want to contribute ? Please considered do a pull request <a href="https://github.com/MathisBarre/next-learningdev">on the github repository</a> or send me your sources here at my email adress!</p>
+        <h3>Roadmap</h3>
+        <ul>
+          <li>Add authentification to implement :</li>
+          <ul>
+            <li>likes to rank sources</li>
+            <li>sort options (by likes, likes/month, language, difficulty...)</li>
+            <li>contribute directly on the website (add & modify sources) </li>
+          </ul>
         </ul>
-        <p>You want to contribute ? Please considered do a pull request <a href="https://github.com/MathisBarre/next-learningdev">on the github repository</a> !</p>
         <h3>The author's contact informations : </h3>
         <ul>
           <li> <strong>Website : </strong><a href="https://mathisbarre.com">mathisbarre.com</a></li>
@@ -21,6 +29,7 @@ export default function Home(props) {
           <li> <strong>Github : </strong><a href="https://github.com/MathisBarre">MathisBarre</a></li>
           <li> <strong>Twitter : </strong><a href="https://twitter.com/LeMathisBarre">@LeMathisBarre</a></li>
         </ul>
+
         <p>This website was made with Nextjs and Host with Vercel</p>
       </main>
     </Layout>
